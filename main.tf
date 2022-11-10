0497a715ad4410dd31548916bb429c947eb4b364
Sebastien Bonnet <49565350+sbonnet-dev@users.noreply.github.com>
2022-11-10 21:56:41 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,14 @@
-resource "azurerm_resource_group" "resource_group" {
-  name     = lower(format("%s%s%s", var.rg_type, var.environment, var.name))
-  location = var.location
+resource "azurerm_storage_account" "storage_account" {
+  name = lower(format("%s%s%s", var.rg_type, var.environment, var.name))
+
+  location                 = var.location
+  resource_group_name      = var.rg_name
+  account_tier             = var.account_tier
+  account_replication_type = var.account_replication_type
+
+  tags = {
+    project     = var.project
+    environment = var.environment
+    owner       = var.owner
+  }
 }
