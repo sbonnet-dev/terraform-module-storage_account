a9b87cdbb27cc538cad00f4238eb4e5b4c9035e5
Sebastien Bonnet <49565350+sbonnet-dev@users.noreply.github.com>
2022-11-10 22:33:44 +0100

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,11 +1,1 @@
-output "resource_group_name" {
-  value = azurerm_resource_group.resource_group.name
-}
 
-output "location" {
-  value = azurerm_resource_group.resource_group.location
-}
-
-output "resource_group_id" {
-  value = azurerm_resource_group.resource_group.id
-}
